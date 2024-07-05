1e39f8dd2a7f80e6c46beb7cef5a67bf314c1e62
Elliot <elliotgriffiss@gmail.com>
2024-07-05 06:58:56 +0100

    Implemented Core Gameplay loop

--- src/Game.ts
+++ src/Game.ts
@@ -13,37 +13,56 @@ import RevealAnimation from "./Components/RevealAnimation/RevealAnimation";
 import SpinButton from "./Components/SpinButton/SpinButton";
 import CreditPanel from "./Components/CreditPanel/CreditPanel";
 
+import setting from './app.json';
+
 class Game extends Container {
-    private _credit: number = 10;
+    private readonly _symbolManager: SymbolManager = null;
+    private readonly _bonus: Bonus = null;
+    private readonly _revealAnimation: RevealAnimation = null;
+    private readonly _creditPanel: CreditPanel = null;
+    private readonly _spinButton: SpinButton = null;
+
+    private _credit: number = setting.startingCredit;
 
     constructor() {
         super();
 
         const background = new Background();
-        const symbolManager = new SymbolManager();
-        const bonus = new Bonus();
-        const revealAnimation = new RevealAnimation();
+        this._symbolManager = new SymbolManager();
+        this._bonus = new Bonus();
+        this._revealAnimation = new RevealAnimation();
+        this._creditPanel = new CreditPanel();
+        this._creditPanel.setText(this._credit);
 
-        const creditPanel = new CreditPanel();
-        creditPanel.setText(this._credit);
-
-        const spinButton = new SpinButton(()=> {this._onSpinButtonPressed()});
+        this._spinButton = new SpinButton(()=> {this._onSpinButtonPressed()});
 
         this.addChild(
             background,
-            symbolManager,
-            bonus,
-            revealAnimation,
-            creditPanel,
-            spinButton
+            this._symbolManager,
+            this._bonus,
+            this._revealAnimation,
+            this._creditPanel,
+            this._spinButton
         );
     }
 
     private _onSpinButtonPressed(): void {
-        void this.playGame();
+        if (this._credit >= setting.stake) {
+            this._credit -= setting.stake;
+            this._creditPanel.setText(this._credit);
+
+            void this.playGame();
+        }
     }
 
-    playGame(): Promise<void> {
+    async playGame(): Promise<void> {
+        this._spinButton.isActive = false;
+
+        await this._revealAnimation.play();
+
+        if (this._credit >= setting.stake) {
+            this._spinButton.isActive = true;
+        }
         return Promise.resolve();
     }
 }
